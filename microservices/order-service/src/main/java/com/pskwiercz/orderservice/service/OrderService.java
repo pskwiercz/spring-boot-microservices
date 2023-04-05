4e2fb646f1874e844e0fbd19c47287d5656f9f5e
Przemek Skwiercz <przemek.skwiercz@gmail.com>
2023-04-05 23:19:22 +0200

    Refactor project - create modules

---
+++ microservices/order-service/src/main/java/com/pskwiercz/orderservice/service/OrderService.java
@@ -0,0 +1,43 @@
+package com.pskwiercz.orderservice.service;
+
+import com.pskwiercz.orderservice.dto.OrderLineItemsDto;
+import com.pskwiercz.orderservice.dto.OrderRequest;
+import com.pskwiercz.orderservice.model.Order;
+import com.pskwiercz.orderservice.model.OrderLineItems;
+import com.pskwiercz.orderservice.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public void placeOrder(OrderRequest orderRequest) {
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+
+        List<OrderLineItems> orderLineItemsList = orderRequest.getOrderLineItemsDtoList()
+                .stream()
+                .map(this::mapToDto)
+                .toList();
+
+        order.setOrderLineItemsList(orderLineItemsList);
+
+        orderRepository.save(order);
+    }
+
+    private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
+        OrderLineItems orderLineItems = new OrderLineItems();
+        orderLineItems.setPrice(orderLineItemsDto.getPrice());
+        orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
+        orderLineItems.setSkuCode(orderLineItemsDto.getSkuCode());
+        return orderLineItems;
+    }
+}
