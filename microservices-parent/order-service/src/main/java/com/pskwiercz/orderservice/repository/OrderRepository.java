9d6d9318cf765ff13362c6ba7906fa2fd47b7189
Przemek Skwiercz <przemek.skwiercz@gmail.com>
2023-04-06 09:32:25 +0200

    Delete old files

--- microservices-parent/order-service/src/main/java/com/pskwiercz/orderservice/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pskwiercz.orderservice.repository;
-
-import com.pskwiercz.orderservice.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
--- microservices-parent/product-service/src/main/java/com/pskwiercz/productservice/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pskwiercz.productservice.repository;
-
-import com.pskwiercz.productservice.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface  ProductRepository extends MongoRepository<Product, String> {
-}
